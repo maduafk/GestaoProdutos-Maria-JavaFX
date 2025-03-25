e252d948c2c2d1df5f6a5f54ce913d12ee32ddb2
Madu <softwaremadu@gmail.com>
2025-03-25 19:48:19 -0300

    mudança calculo imc

--- src/main/java/com/example/sla/Application.java
+++ src/main/java/com/example/sla/Application.java
@@ -9,7 +9,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Calculo IMC by Madu!");
+        stage.setTitle("Calculo IMC by Mad!");
         stage.setScene(scene);
         stage.show();
     }
